796dee723fc5093a666c9175a9435cae486ea714
DMish <dsmishler@gmail.com>
2022-07-18 18:11:53 -0400

    adding my own coup agent

---
+++ practice_7/practice_7.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jul 18 11:42:52 2022
+
+@author: dsmishler
+"""
+
+# Note: this is the final practice in the coup class. Be ready for the arena
+# next class
+
+
+
+# Problem 1
+# Prepare your agent to the maximum effectiveness to duel against other coup
+# agents in the arena
